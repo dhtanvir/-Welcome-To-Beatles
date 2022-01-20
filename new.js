a2341b841b171642f6a85f3e1516d25a583e2675
MD Tanvirul Islam <57485202+dhtanvir@users.noreply.github.com>
2022-01-21 00:47:04 +0600

    Add files via upload

---
+++ new.js
@@ -0,0 +1,56 @@
+
+
+$(document).ready(function() {
+  $("#hide").click(function(){
+    $(".p").show();
+  });
+  $("#hide").dblclick(function(){
+    $(".p").hide();
+  });
+// 
+    $("#img_hide").click(function () { 
+        $("#img_show").show();
+        $("#img_show2").show();
+        $("#img_show3").show();
+        $("#img_show4").show();
+        $("#img_show5").show();
+    });
+     $("#img_hide").dblclick(function () { 
+        $("#img_show").hide();
+        $("#img_show2").hide();
+        $("#img_show3").hide();
+        $("#img_show4").hide();
+        $("#img_show5").hide();
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
